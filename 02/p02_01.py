8150e1a0d8495952eff6686e23071014698d0e90
Carlos Garcia <cgarciams@hotmail.com>
2026-07-07 07:15:03 +0200

    02-01

--- 02/p02_01.py
+++ 02/p02_01.py
@@ -1,15 +1,19 @@
 import torch
 
-print(f"\nPyTorch version {torch.__version__}")
+print("\nSoftware and hardware evaluation:")
 
+# Software - PyTorch version
+print(f"- PyTorch version: {torch.__version__}")
+
+# Hardware - Each kind (CUDA, Apple or Intel) has a different eval. function that returns True or False
 if torch.cuda.is_available():
-    print(f"CUDA GPU: {torch.cuda.get_device_name(0)}")
+    print(f"- CUDA GPU: {torch.cuda.get_device_name(0)}")
 
 elif torch.xpu.is_available():
-    print(f"Intel GPU: {torch.xpu.get_device_name(0)}")
+    print(f"- Intel XPU: {torch.xpu.get_device_name(0)}")
 
 elif torch.backends.mps.is_available():
-    print("Apple Silicon GPU")
+    print("- Apple Silicon MPS SoC")
 
 else:
     print("Only CPU")
