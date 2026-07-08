720d793bfb25eedc3c708f1325d80888ec56e4cf
Carlos Garcia <cgarciams@hotmail.com>
2026-07-08 07:11:35 +0200

    02-02

--- 02/p02_02.py
+++ 02/p02_02.py
@@ -1,23 +1,27 @@
 # 2.4 Preparing input texts for LLMs
 
 from pathlib import Path
+from rich.console import Console
 from reasoning_from_scratch.qwen3 import Qwen3Tokenizer
 
 
 # Creates "qwen" folder and downloads "tokenizer-base.json" 
-download_qwen3_small(kind="base", tokenizer_only=True, out_dir="qwen3")
+#download_qwen3_small(kind="base", tokenizer_only=True, out_dir="qwen3")
+
+console = Console()
 
 # Crea los objetos tokenizer_path y tokenizer. En Python el operador / se traduce automáticamente por el "path separator" de Linux, macOS o Windows.
 tokenizer_path = Path("qwen3") / "tokenizer-base.json"
 tokenizer = Qwen3Tokenizer(tokenizer_file_path=tokenizer_path)
+console.print(f"\nTokenizer created (from {tokenizer_path})\n", style="gold1")
+
 
 # Test the tokernizer: Encode
-prompt = "The quick brown fox jumps over the lazy dog"
+prompt = "The quick brown fox jumps over the lazy dog. Milana bonita."
 input_token_ids_list = tokenizer.encode(prompt)
 text = tokenizer.decode(input_token_ids_list)
-print()
-print(text)
-print()
+print(f"Input: {text}\n")
+print("Token IDs:")
 for i in input_token_ids_list:
     print(f"{i} --> {tokenizer.decode([i])}")
 print()
