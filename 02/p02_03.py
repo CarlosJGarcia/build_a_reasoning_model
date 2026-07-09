925eeaeb4cf61a3c939564ea440b3c71c27ccf76
Carlos Garcia <cgarciams@hotmail.com>
2026-07-09 07:36:30 +0200

    02-03-04

--- 02/p02_03.py
+++ 02/p02_03.py
@@ -11,7 +11,7 @@ def get_device(enable_tensor_cores=True):
     if torch.cuda.is_available():
         # Enable GPU
         device = torch.device("cuda")
-        print("Using NVIDIA CUDA GPU")
+        print("Device: NVIDIA CUDA GPU")
 
         if enable_tensor_cores:
             # Enable Tensor Cores for faster matrix multiplications
@@ -25,16 +25,16 @@ def get_device(enable_tensor_cores=True):
 
     elif torch.backends.mps.is_available():
         device = torch.device("mps")
-        print("Using Apple Silicon GPU (MPS)")
+        print("Device: Apple Silicon GPU (MPS)")
 
     elif torch.xpu.is_available():
         device = torch.device("xpu")
-        print("Using Intel GPU")
+        print("Device: Intel GPU")
 
     else:
         # Enable CPU. This can be used as well to bypass the GPU and force using the CPU.
         device = torch.device("cpu")
-        print("Using CPU")
+        print("Device: CPU")
 
     return device
 
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     model = Qwen3Model(QWEN_CONFIG_06_B)
     model.load_state_dict(torch.load(model_path))
     model.to(device)
-    console.print(f"Qwen3 loaded in VRAM (from {model_path})\n", style="gold1", highlight=False)
+    console.print(f"Qwen3 loaded in ({device}) GPU and VRAM (from {model_path})\n", style="gold1", highlight=False)
 
     # Shows model architecture
     console.print("Model architecture:", style="gold1")
--- 02/p02_04.py
+++ 02/p02_04.py
@@ -1,15 +1,30 @@
+# 2.6 Understanding the sequential LLM text generation process
+
 import torch
 from pathlib import Path
+from p02_03 import get_device
 from rich.console import Console
 from reasoning_from_scratch.qwen3 import Qwen3Tokenizer, Qwen3Model, QWEN_CONFIG_06_B
 
-from p02_03 import get_device
-
-# Crea el objeto tokenizer
-tokenizer_path = Path("qwen3") / "tokenizer-base.json"
-tokenizer = Qwen3Tokenizer(tokenizer_file_path=tokenizer_path)
 
 console = Console()
+
+# Load the tokenizer
+tokenizer_path = Path("qwen3") / "tokenizer-base.json"
+tokenizer = Qwen3Tokenizer(tokenizer_file_path=tokenizer_path)
+console.print(f"\nTokenizer created (from {tokenizer_path})", style="gold1")
+
+# Load the model
+device = get_device()
+model_path = Path("qwen3") / "qwen3-0.6B-base.pth"
+model = Qwen3Model(QWEN_CONFIG_06_B)
+model.load_state_dict(torch.load(model_path))
+model.to(device)
+console.print(f"Qwen3 loaded in ({device}) GPU and VRAM (from {model_path})\n", style="gold1", highlight=False)
+
+
+
+"""
 prompt = "Explain large language models."
 input_token_ids_list = tokenizer.encode(prompt)
 print(f"\nNumber of input tokens: {len(input_token_ids_list)}")
@@ -21,12 +36,6 @@ input_tensor = torch.tensor(input_token_ids_list)
 input_tensor_fmt = input_tensor.unsqueeze(0)  
 input_tensor_fmt = input_tensor_fmt.to(device)
 
-# Load the model
-model_path = Path("qwen3") / "qwen3-0.6B-base.pth"
-model = Qwen3Model(QWEN_CONFIG_06_B)
-model.load_state_dict(torch.load(model_path))
-model.to(device)
-console.print("Qwen3 loaded in VRAM\n", style="gold1")
 
 # Inference
 with torch.inference_mode():
@@ -38,3 +47,4 @@ last_token = output_tensor_fmt[-1]
 print(last_token)
 console.print(f"{tokenizer.decode([20286])}", style="white")
 print()
+"""
