676d1eed81151058c8545fa9f2779341d7206fae
Carlos Garcia <cgarciams@hotmail.com>
2026-07-10 06:35:34 +0200

    02-04

--- 02/p02_04.py
+++ 02/p02_04.py
@@ -15,7 +15,7 @@ tokenizer_path = Path("qwen3") / "tokenizer-base.json"
 tokenizer = Qwen3Tokenizer(tokenizer_file_path=tokenizer_path)
 console.print(f"\nTokenizer created (from {tokenizer_path})", style="gold1")
 
-# Load the model
+# Load the model and send to GPU
 device = get_device()
 model_path = Path("qwen3") / "qwen3-0.6B-base.pth"
 model = Qwen3Model(QWEN_CONFIG_06_B)
@@ -31,7 +31,7 @@ print(f"Prompt: {prompt}")
 print(f"Number of input tokens: {len(input_token_ids_list)}")
 
 
-# Load the input tensor
+# Convert tokens to tensor and send to GPU
 input_tensor = torch.tensor(input_token_ids_list)
 input_tensor_fmt = input_tensor.unsqueeze(0)  
 input_tensor_fmt = input_tensor_fmt.to(device)
@@ -41,9 +41,9 @@ input_tensor_fmt = input_tensor_fmt.to(device)
 with torch.inference_mode():
     output_tensor = model(input_tensor_fmt)  
 output_tensor_fmt = output_tensor.squeeze(0) 
-print(f"Formatted Output tensor shape: {output_tensor_fmt.shape}")
+print(f"Formatted output tensor shape: {output_tensor_fmt.shape}")
 
 last_token = output_tensor_fmt[-1]
-console.print(f"Last token. {tokenizer.decode([20286])}", style="white")
+console.print(f"Last token: {tokenizer.decode([20286])}", style="white")
 print()
 
